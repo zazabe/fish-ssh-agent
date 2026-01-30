e412e10264c686dc408c1fb05cafe4b5f55cb194
Michel <401598+zazabe@users.noreply.github.com>
2026-01-30 18:18:38 +0700

    Refine ssh-agent initialization for SSH_AGENT_OPTS

--- conf.d/fish-ssh-agent.fish
+++ conf.d/fish-ssh-agent.fish
@@ -2,8 +2,14 @@ if test -z "$SSH_ENV"
     set -xg SSH_ENV $HOME/.ssh/environment
 end
 
-if not __ssh_agent_is_started
-    __ssh_agent_start
+function __fish_ssh_agent_init --on-event fish_prompt
+    if not set -q __fish_ssh_agent_initialized
+        if not __ssh_agent_is_started
+            __ssh_agent_start
+        end
+        set -g __fish_ssh_agent_initialized 1
+        functions -e __fish_ssh_agent_init
+    end
 end
 
 function __on_fish_exit --on-event fish_exit
